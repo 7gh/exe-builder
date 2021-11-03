7a1bb1bb9b6be2807e3375b4b2dbcbf776c90314
7gh <88673481+7gh@users.noreply.github.com>
2021-11-02 21:42:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+ok = input('Enter the file name > ')
+try:
+    os.system(f'pyinstaller --onefile {ok}')
+except:
+    os.system(f'py -m pip install pyinstaller')
+    os.system(f'pyinstaller --onefile {ok}')
